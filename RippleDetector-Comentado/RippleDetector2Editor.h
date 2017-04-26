d326b18aa2ef86eb1750ff1c781792c408ecdcd5
ramonbhaskara <ramonbhaskara@gmail.com>
2017-04-26 20:30:45 -0300

    Create RippleDetector2Editor.h

---
+++ RippleDetector-Comentado/RippleDetector2Editor.h
@@ -0,0 +1,114 @@
+//****************************************************> significa que veio do Bandpass
+
+#ifndef __RippleDETECTOR2EDITOR_H_136829C6__
+#define __RippleDETECTOR2EDITOR_H_136829C6__
+
+#include <EditorHeaders.h>
+
+class RippleInterface;
+class RippleDetector2;
+class ElectrodeButton;
+//****************************************************
+//class FilterViewport;
+//****************************************************
+
+class RippleDetector2Editor : public GenericEditor,
+    public ComboBox::Listener//,
+//****************************************************
+//    public Label::Listener
+//****************************************************
+{
+public:
+    RippleDetector2Editor(GenericProcessor* parentNode, bool useDefaultParameterEditors);
+    virtual ~RippleDetector2Editor();
+
+    void buttonEvent(Button* button);
+
+    void comboBoxChanged(ComboBox* c);
+
+    void updateSettings();                                                                                    //Necessário
+
+    void saveCustomParameters(XmlElement* xml);
+    void loadCustomParameters(XmlElement* xml);
+    
+//****************************************************
+//    void labelTextChanged(Label* label);
+
+    void setDefaults(double lowCut, double highCut);
+//****************************************************
+
+private:
+
+//    ScopedPointer<ComboBox> detectorSelector;
+
+    ScopedPointer<UtilityButton> plusButton;
+
+    void addDetector();
+
+    OwnedArray<RippleInterface> interfaces;
+
+    int previousChannelCount;
+
+    Array<Colour> backgroundColours;
+    
+//****************************************************
+    String lastHighCutString;
+    String lastLowCutString;
+
+    ScopedPointer<Label> highCutLabel;
+    ScopedPointer<Label> lowCutLabel;
+/*
+    ScopedPointer<Label> highCutValue;
+    ScopedPointer<Label> lowCutValue;  */
+//****************************************************
+
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(RippleDetector2Editor);
+
+};
+
+class RippleInterface : public Component,
+    public ComboBox::Listener,
+    public Button::Listener
+{
+public:
+    RippleInterface(RippleDetector2*, Colour, int);
+    ~RippleInterface();
+
+    void paint(Graphics& g);
+
+    void comboBoxChanged(ComboBox*);
+    void buttonClicked(Button*);
+
+    void updateChannels(int);
+
+    void setPhase(int);
+    void setInputChan(int);
+    void setOutputChan(int);
+    void setGateChan(int);
+
+    int getPhase();
+    int getInputChan();
+    int getOutputChan();
+    int getGateChan();
+
+private:
+
+    Colour backgroundColour;
+
+    Path sineWave;
+    Font font;
+
+    int idNum;
+
+    RippleDetector2* processor;
+
+    OwnedArray<ElectrodeButton> phaseButtons;
+
+    ScopedPointer<ComboBox> inputSelector;
+    ScopedPointer<ComboBox> gateSelector;
+    ScopedPointer<ComboBox> outputSelector;
+
+};
+
+#endif  // __RippleDETECTOR2EDITOR_H_136829C6__
